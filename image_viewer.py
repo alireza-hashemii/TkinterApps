3adfd2c2efb8cf3e14880136db22a108857d44dc
1alirezahashemi1 <pyalireza7@gmail.com>
2023-09-08 15:38:54 +0330

    App viewer app - basic structure is done. 107

--- image_viewer.py
+++ image_viewer.py
@@ -4,22 +4,38 @@ from PIL import ImageTk, Image
 window = Tk()
 window.title("Image Gallery")
 window.iconbitmap("E:\Programming\Tkinter\imgicon\ic3.ico")
-# window.geometry("700x400")
 
 
-image1 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza.png"))
-image2 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza2.png"))
-image3 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza3.png"))
-image4 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza4.png"))
-image5 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza5.png"))
+
+image1 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza.png"),name="image1")
+image2 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza2.png"),name="image2")
+image3 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza3.png"),name="image3")
+image4 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza4.png"),name="image4")
+image5 = ImageTk.PhotoImage(Image.open(r"imgicon\alireza5.png"),name="image5")
 
 image_list = [image1, image2, image3, image4, image5]
 
-mimage = Label(image=image1,width=550,height=470).grid(row=0,column=0,columnspan=3)
+
+current_image = Label(image=image1)
+current_image.grid(row=0, column=0, columnspan=3)
+
+def back():
+    pass
+
+def forward(image_number:int):
+    global current_image
+    global previous_button
+    global next_button 
+
+    current_image.grid_forget()
+    current_image = Label(image= image_list[image_number - 1])
+    next_button = Button(window, text="<<", command=lambda: forward(image_number + 1)).grid(row=1,column=0)
+    previous_button = Button(window, text=">>", command=lambda: forward(image_number - 1)).grid(row=1,column=2)
 
 
-next_button = Button(window, text="<<").grid(row=1,column=0)
-previous_button = Button(window, text=">>").grid(row=1,column=2)
+
+previous_button = Button(window, text="<<", command=back).grid(row=1,column=0)
+next_button = Button(window, text=">>L", command=lambda: forward(2)).grid(row=1,column=2)
 quit_button = Button(window,text="QUIT", width=10,height=3,bg="red", fg="black", command=window.quit).grid(row=1,column=1)
 
 window.mainloop()
