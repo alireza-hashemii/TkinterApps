a3667147c46884ad67f9d2aa5e3459e1a1dfb59d
1alirezahashemi1 <pyalireza7@gmail.com>
2023-09-08 14:18:02 +0330

    image & icon and Exit button. 105

--- icon_buttons_images.py
+++ icon_buttons_images.py
@@ -1,9 +1,16 @@
 from tkinter import *
+from PIL import ImageTk, Image
+
 window = Tk()
 window.title("Icon Learning.")
 window.iconbitmap("E:\Programming\Tkinter\imgicon\ic2.ico")
+window.geometry("700x400")
 
 
+image = ImageTk.PhotoImage(Image.open(r"imgicon\alireza.png"))
+mylabelimg = Label(image=image)
+mylabelimg.pack()
 
+quit_button = Button(window,text="QUIT", width=12,height=15,bg="red", fg="black", command=window.quit).pack()
 
 window.mainloop()
