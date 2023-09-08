ed8939e9ff7aedb1e0d3287449b053485d924fec
1alirezahashemi1 <pyalireza7@gmail.com>
2023-09-08 13:37:16 +0330

    APP1 - Calculator - All the mathematical operation buttons are working perfectly. 102

--- calculator_app.py
+++ calculator_app.py
@@ -20,16 +20,52 @@ def add(number):
 def clear():
     entry.delete(0,END)
 
+
 def add_button():
     global first_number
+    global math_operation 
+    math_operation = "addition"
     first_number = entry.get()
     entry.delete(0,END)
+
 
 def equal_button():
     global second_number
     second_number = entry.get()
     entry.delete(0,END)
-    entry.insert(0, int(first_number) + int(second_number))
+    
+    if math_operation == "addition":
+        entry.insert(0, int(first_number) + int(second_number))
+    elif math_operation == "subtraction":
+        entry.insert(0, int(first_number) - int(second_number))
+    elif math_operation == "division":
+        entry.insert(0, int(first_number) / int(second_number))
+    elif math_operation == "multiplication":
+        entry.insert(0, int(first_number) * int(second_number))
+
+
+def subtract_button():
+    global first_number
+    global math_operation 
+    math_operation = "subtraction"
+    first_number = entry.get()
+    entry.delete(0,END)
+
+
+def divide_button():
+    global first_number
+    global math_operation 
+    math_operation = "division"
+    first_number = entry.get()
+    entry.delete(0,END)
+
+
+def multiply_button():
+    global first_number
+    global math_operation 
+    math_operation = "multiplication"
+    first_number = entry.get()
+    entry.delete(0,END)
 
 # keys of calculator are here
 button1 = Button(window, text="1", padx=18, pady=16 , command=lambda: add(1), bg="#71f6f6").grid(row=1, column=0)
@@ -47,9 +83,14 @@ button9 = Button(window, text="0", padx=18, pady=16, command=lambda: add(0), bg=
 
 
 
-button_add = Button(window, text="+", padx=18, pady=16, bg="#6ef5ba",command=add_button).grid(row=5, column=0)
-button_add = Button(window, text="Clear", padx=36, pady=16, bg="green",command=clear).grid(row=4, column=1, columnspan=2)
+button_add = Button(window, text="+", padx=18, pady=16, bg="#6ef5ba", command=add_button).grid(row=5, column=0)
+button_add = Button(window, text="Clear", padx=36, pady=16, bg="green", command=clear).grid(row=4, column=1, columnspan=2)
 button_equal = Button(window, text="=", padx=45, pady=16, bg="#fb36ef", command=equal_button).grid(row=5, column=1, columnspan=2)
+
+
+button_subtract = Button(window, text="-", padx=19, pady=16, bg="#6ef5ba", command=subtract_button).grid(row=6, column=0)
+button_multiply = Button(window, text="x", padx=18, pady=16, bg="#6ef5ba", command=multiply_button).grid(row=6, column=1)
+button_divide = Button(window, text="%", padx=18, pady=16, bg="#6ef5ba", command=divide_button).grid(row=6, column=2)
 
 
 window.mainloop()
