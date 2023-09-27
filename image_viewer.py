e324e4ab29e60e3c97ea264e3d582efbc993e6bf
1alirezahashemi1 <pyalireza7@gmail.com>
2023-09-27 16:55:34 +0330

    adding status bar

--- image_viewer.py
+++ image_viewer.py
@@ -5,7 +5,6 @@ root = Tk()
 root.title('Codemy.com Image Viewer')
 root.iconbitmap('imgicon/ic3.ico')
 
-
 my_img1 = ImageTk.PhotoImage(Image.open("imgicon/alireza.png"))
 my_img2 = ImageTk.PhotoImage(Image.open("imgicon/alireza2.png"))
 my_img3 = ImageTk.PhotoImage(Image.open("imgicon/alireza3.png"))
@@ -19,6 +18,8 @@ image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
+status = Label(root, text=f"Image 1 of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
+
 def forward(image_number):
 	global my_label
 	global button_forward
@@ -28,6 +29,8 @@ def forward(image_number):
 	my_label = Label(image=image_list[image_number-1])
 	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
 	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+	status = Label(root, text=f"Image {image_number} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
+
 	my_label.grid(row=0, column=0, columnspan=3)
 	if image_number == 5:
 		button_forward = Button(root, text=">>", state=DISABLED)
@@ -35,6 +38,8 @@ def forward(image_number):
 	my_label.grid(row=0, column=0, columnspan=3)
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
+	status.grid(row=2, column=0, columnspan=3,sticky=W+E)
+
 
 def back(image_number):
 	global my_label
@@ -45,6 +50,8 @@ def back(image_number):
 	my_label = Label(image=image_list[image_number-1])
 	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
 	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+	status = Label(root, text=f"Image {image_number} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
+
 
 	if image_number == 1:
 		button_back = Button(root, text="<<", state=DISABLED)
@@ -52,6 +59,8 @@ def back(image_number):
 	my_label.grid(row=0, column=0, columnspan=3)
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
+	status.grid(row=2, column=0, columnspan=3,sticky=W+E)
+
 
 
 
@@ -63,5 +72,6 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
 button_forward.grid(row=1, column=2)
+status.grid(row=2, column=0, columnspan=3,sticky=W+E)
 
 root.mainloop()
