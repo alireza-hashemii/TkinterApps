dbb33f9cb12d86eac0bf7bb6742c637653262c69
alireza-hashemii <alirezahashemi.dev@gmail.com>
2023-09-30 13:54:43 +0330

    finish radio button

--- radio_button.py
+++ radio_button.py
@@ -2,18 +2,33 @@ from tkinter import *
 
 window = Tk()
 
-opt = IntVar()
-opt.set(2)
+MODES = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Onion", "Onion"),
+    ("Margarita", "Margarita"),
+]
+
+opt = StringVar()
+opt.set("Pepperoni")
+
+for text, mode in MODES:
+    Radiobutton(window, text=text, variable=opt, value=mode).pack()
+
+
+# opt = IntVar()
+# opt.set(2)
 
 def clicked(val):
     global mylabel
     mylabel = Label(window, text=val).pack()
 
-Radiobutton(window, text="Option-1", variable=opt,
-            value=1, command=lambda: clicked(1)).pack()
-Radiobutton(window, text="Option-2", variable=opt,
-            value=2, command=lambda: clicked(2)).pack()
+# Radiobutton(window, text="Option-1", variable=opt,
+#             value=1, command=lambda: clicked(1)).pack()
+# Radiobutton(window, text="Option-2", variable=opt,
+#             value=2, command=lambda: clicked(2)).pack()
 
-
-mylabel = Label(window, text=opt.get()).pack()
+choosable_button = Button(window, text="Wanna know the mode?",
+                    padx=15, pady=12,command=lambda: clicked(opt.get())).pack()
+# mylabel = Label(window, text=opt.get()).pack()
 window.mainloop()
