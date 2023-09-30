4a7339557fda1aae4fb369b725633a0a8163a032
alireza-hashemii <alirezahashemi.dev@gmail.com>
2023-09-30 15:05:40 +0330

    dialogbox - we've had a bug

---
+++ dialogbox.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk,Image
+
+
+window = Tk()
+
+def selection():
+    global lbl
+    global filename
+    filename = window.filename = filedialog.askopenfilename(initialdir="Pictures/Camera Roll",
+                        title="select a file",filetypes=(("png files","*.png"),("all filess",("*"))))
+
+    sub_window = Toplevel()
+    lbl = Label(sub_window, image=filename).pack()
+
+
+
+
+button_ = Button(window, text="wanna choose a Photo?", bg="#bfeb3e" , padx=30, pady=26, command=selection).pack()
+
+window.mainloop()
