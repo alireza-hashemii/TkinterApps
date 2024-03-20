06da4818163cb3cb7dee2bf0502af81dc8db2f18
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-20 15:07:28 +0330

    need bugfix - labels aren't working well

--- weather_forecast.py
+++ weather_forecast.py
@@ -44,8 +44,37 @@ Logo_image = PhotoImage(file="imgicon/logo.png")
 logo = Label(image=Logo_image)
 logo.place(x=150, y=100)
 
-
+# Bottom box
+Frame_image = PhotoImage(file="imgicon/box.png")
+frame_myimage = Label(image=Frame_image)
+frame_myimage.pack(padx=5 , pady=5,side=BOTTOM )
 root.mainloop()
+
+# Label
+
+label1=Label(root, text="WIND", font=('Helvetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label1.place(x=120, y=400)
+
+label2=Label(root, text="HUMIDITY", font=('Helvetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label2.place(x=250, y=400)
+
+label3=Label(root, text="DESCRIPTION", font=('Helvetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label3.place(x=430, y=400)
+
+label4=Label(root, text="PRESSURE", font=('Helvetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label4.place(x=650, y=400)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
